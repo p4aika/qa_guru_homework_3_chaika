83713dc7993f8d2f2a6ee459ff2949eb4e94fd46
Pavel Chaika <mrpavel.chaika@gmail.com>
2023-03-01 01:30:49 +0300

    delete_test_google_should_not_find_text

--- test_first.py
+++ test_first.py
@@ -12,8 +12,3 @@ def test_google_should_find_text(set_browser_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
-def test_google_should_not_find_text(set_browser_size):
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('testgoogleshouldnotfindtext').press_enter()
-    browser.element('#topstuff').should(have.text('По запросу testgoogleshouldnotfindtext ничего не найдено.'))
